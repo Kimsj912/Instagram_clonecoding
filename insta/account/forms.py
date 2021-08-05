6c21e52e62743661041c23a6218e508b50f23f04
portfolio_1st <aa991122aa@gmail.com>
2021-08-05 18:34:01 +0900

    login과 sign in 페이지 목표 css까지 완성 (내일은 프로필 화면 만들 생각 - home을 변경할 것)

--- insta/account/forms.py
+++ insta/account/forms.py
@@ -4,9 +4,23 @@ from .models import userInfo
 class UserForm(forms.ModelForm):
     class Meta:
         model=userInfo
-        fields =['username','email','password']
+        fields =['email','name','username','password']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder']=' 이메일을 입력하세요'
+        self.fields['name'].widget.attrs['placeholder']=' 이름'
+        self.fields['username'].widget.attrs['placeholder']=' 사용자 이름'
+        self.fields['password'].widget.attrs['placeholder']=' 비밀번호'
+        
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = userInfo
-        fields = ['username', 'password']
+        fields = ['email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder']=' 이메일을 입력하세요'
+        self.fields['password'].widget.attrs['placeholder']=' 비밀번호'
+        
--- insta/account/models.py
+++ insta/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import CharField, EmailField, IntegerField
+from django.urls import reverse
 
 class userInfo(models.Model):
     email = models.EmailField(max_length=254)
@@ -7,3 +8,6 @@ class userInfo(models.Model):
     name=models.CharField(max_length=40)
     username=models.CharField(max_length=40)
     password = models.CharField(max_length=50)
+
+    def get_absolute_url(self): # redirect시 활용
+        return reverse('profile:profile_detail', args=[self.id])
